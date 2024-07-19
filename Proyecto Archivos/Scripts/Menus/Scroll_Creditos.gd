045556d472da2697ec966aaa74d4dbfcc86dbd40
Nestor Alejandro Morales Martinez <moralesnestoralejandro@gmail.com>
2024-07-19 17:45:46 -0500

    Cambios
    Cambios Creditos

--- Proyecto Archivos/Scripts/Menus/Scroll_Creditos.gd
+++ Proyecto Archivos/Scripts/Menus/Scroll_Creditos.gd
@@ -2,16 +2,16 @@ extends ScrollContainer
 
 # Llamado cuando se mueve el mouse dentro del ScrollContainer
 var movimiento = 0
-
+var velocidad = 20
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		# Obtén el desplazamiento del mouse en píxeles
 		var delta = event.relative
-		if movimiento >= 0 and movimiento<= 640:
-			movimiento += delta.y * 5
-		elif movimiento > 640:
-			movimiento -= 5
+		if movimiento >= 0 and movimiento<= 838:
+			movimiento += delta.y * velocidad
+		elif movimiento > 838:
+			movimiento -= velocidad
 		elif movimiento < 0:
-			movimiento += 5
+			movimiento += velocidad
 		# Ajusta la posición de desplazamiento del ScrollContainer
 		set_v_scroll(movimiento)
